ff07e8f6229f563524ccbbebf4ea39e925e06d4a
Mathias Mustonen <mathias.mustonen@gmail.com>
2021-01-10 20:32:00 +0200

    CustomButton-component template

--- Custom UI Event System/Assets/Scripts/CustomEventSystem.cs
+++ Custom UI Event System/Assets/Scripts/CustomEventSystem.cs
@@ -1,31 +1,77 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CustomEventSystem : EventSystem
 {
-    public void DebugMessage(GameObject gameObject)
-    {
-        Debug.Log(gameObject.name, gameObject);
-    }
+    private Selectable[] _selectables = default;
+    private GameObject _previousSelectedObject = default;
 
-    public void DebugMessage(string message)
-    {
-        Debug.Log(message);
-    }
+    [SerializeField] private string _submitLabel = "Submit";
+    [SerializeField] private string _cancelLabel = "Cancel";
 
+    public bool IsSubmitButtonUp { get => Input.GetButtonUp(_submitLabel); }
+    public bool IsCancelButtonUp { get => Input.GetButtonUp(_cancelLabel); }
+    public static CustomEventSystem Instance { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Initialize();
+    }
     protected override void Start()
     {
         base.Start();
 
-        if(firstSelectedGameObject == null)
-        {
-            SetSelectedGameObject(gameObject);
-        }
+        SetSelectedObject(firstSelectedGameObject);
     }
-
     protected override void Update()
     {
         base.Update();
 
+        if(IsCancelButtonUp)
+        {
+            SetSelectedObject(null);
+        }
+
+        if(currentSelectedGameObject != null) return;
+
+        if(Input.anyKeyDown)
+        {
+            SetSelectedObject(_previousSelectedObject != null ? _previousSelectedObject : GetFirstActiveSelectedObject());
+        }
+    }
+
+    private void Initialize()
+    {
+        _selectables = FindObjectsOfType<Selectable>();
+
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void SetSelectedObject(GameObject gameObject)
+    {
+        _previousSelectedObject = currentSelectedGameObject;
+
+        SetSelectedGameObject(gameObject);
+    }
+    private GameObject GetFirstActiveSelectedObject()
+    {
+        foreach(var selectble in _selectables)
+        {
+            if(selectble.gameObject.activeInHierarchy)
+            {
+                return selectble.gameObject;
+            }
+        }
+
+        return null;
     }
 }
